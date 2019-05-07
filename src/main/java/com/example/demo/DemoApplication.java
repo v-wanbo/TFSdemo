34f4b71672efeb48453a20c2b66dda3ef3fa18ad
v-wanbo <49471307+v-wanbo@users.noreply.github.com>
2019-05-07 17:30:10 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	 // 2019/02/07
 	public static void main(String[] args) { 
 		System.out.println("DevOps 测试！！！！！！！Git提交 ------------------------- ");  
 		
