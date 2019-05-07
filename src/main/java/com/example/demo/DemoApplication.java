2ada305c93c39538120b2f94971ba2874df91a23
v-wanbo <victory948@sina.com>
2019-05-07 16:25:01 +0800

    git commint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("DevOps 测试！！！！！！！Git提交 ");
-=======
-		System.out.println("DevOps 测试！！！！！！！ ------   ");
->>>>>>> 8a726b9d17b07544929407611799febb6d25ba5c
+	public static void main(String[] args) { 
+		System.out.println("DevOps 测试！！！！！！！Git提交 ");  
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
