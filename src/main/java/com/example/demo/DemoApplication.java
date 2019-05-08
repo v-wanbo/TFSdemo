055ae093778c9e5805c7af4ea8e40be6c2e6cbba
v-wanbo <49471307+v-wanbo@users.noreply.github.com>
2019-05-08 14:05:12 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	 // 2019/02/07
 	public static void main(String[] args) { 
 		int i=5*5*5*5*5;  
-		System.out.println("输出：\n \n \n \n);
+		System.out.println("输出：\n \n \n \n");
 		System.out.println("2019 / 05 /08 正在测试Build ------------------------- 等 = "+i); 
 		SpringApplication.run(DemoApplication.class, args); 
 	}
