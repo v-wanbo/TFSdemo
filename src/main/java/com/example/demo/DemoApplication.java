1013dd3dc538a969dc3cd64ba7eb324f0e85ffd7
v-wanbo <49471307+v-wanbo@users.noreply.github.com>
2019-05-08 10:23:15 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	 // 2019/02/07
 	public static void main(String[] args) { 
+		System.out.println("输出：\n "); 
 		System.out.println("2019 / 05 /08 正在测试Build ------------------------- "); 
 		SpringApplication.run(DemoApplication.class, args);
 	}
